cbcf0386c912039db25b484372a97139c91fea23
tenbae <138585833+tenbae@users.noreply.github.com>
2023-10-03 08:06:53 +0900

    10030806

--- src/subject2/script/reference.js
+++ src/subject2/script/reference.js
@@ -1,38 +1,8 @@
 const reference = [
   {
-    author: "손연숙",
-    title: "사주의 오행분포가 성격형성에 미치는 영향",
-    year: "2008",
-    sourceTitle: "국내석사학위논문 국제문화대학원대학교",
-  },
-  {
-    author: "김석란",
-    title: "사주와 MBTI 성격이론과의 상관관계 연구",
-    year: "2007",
-    sourceTitle: "국내석사학위논문 국제문화대학원대학교",
-  },
-  {
-    author: "김경희",
-    title: "우리 삶 속의 음양오행에 대한 현대적 고찰",
-    year: "2015. 12.",
-    sourceTitle: "한국동서철학회논문집 동서철학연구 제78호",
-  },
-  {
-    author: "백가경 기자 강헌(음악 평론가이자 명리 연구가)",
-    title: "디자이너라면 목, 금, 토",
-    year: "2017년 2월호",
-    sourceTitle: "디자인하우스, 월간디자인 ",
-  },
-  {
-    author: "정혜경 기자",
-    title: "젊은 동년배들이 사주를 배우는 이유",
-    year: "2022년 2월",
-    sourceTitle: "SBS News",
-  },
-  {
-    author: "한겨례 21",
-    title: "나의 운명은 내가 읽는 것",
-    year: "레드기획 1098호",
+    author: "",
+    title: "",
+    year: "",
     sourceTitle: "",
   },
 ];
